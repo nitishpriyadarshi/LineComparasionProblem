3d9008d0383dff9355955c57983c30b7ff79abfb
nitishpriyadarshi <nitish8152@gmail.com>
2022-08-16 10:36:14 +0530

    welcome

---
+++ src/Bridgelabz/LineComparison.java
@@ -0,0 +1,8 @@
+package Bridgelabz;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to The Line Comparison Computation Program");
+    }
+}
+
